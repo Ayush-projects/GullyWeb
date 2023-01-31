58ceeb80552fd3908113e925008fb33ee7062638
Ayush <kayush479@gmail.com>
2023-01-31 14:14:33 +0530

    ss

--- Routes/Main.js
+++ Routes/Main.js
@@ -9,6 +9,15 @@ Router.get("/", (req,res)=>{
     res.render("index")
 })
 
+Router.get("/:city/:state", (req,res)=>{
+    let city = req.params.city
+    let state = req.params.state
+
+
+
+
+    res.json({city, state})
+})
 
 Router.get("/test", (req,res)=>{
     res.send("/test")
